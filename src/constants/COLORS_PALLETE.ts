7dde9998c2d8d5603244e94c1e559c9d5a5c28a0
Lucas Viana (Bau) <contato@lucasviana.dev>
2021-03-07 20:04:16 -0300

    :construction: adds COLORS_PALLETE constant

---
+++ src/constants/COLORS_PALLETE.ts
@@ -0,0 +1,11 @@
+/**
+ * Paleta de cores utilizada no projeto
+ */
+const COLORS_PALLETE = {
+  primary: "#16c79a",
+  secondary: "#11698e",
+  background: "#19456b",
+  brighter: "#f8f1f1",
+};
+
+export default COLORS_PALLETE;
