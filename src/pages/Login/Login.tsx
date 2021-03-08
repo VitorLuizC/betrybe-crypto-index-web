6fd6e905abe979e3ae80dc1f581ee5d3d28f12df
Lucas Viana (Bau) <contato@lucasviana.dev>
2021-03-07 23:20:25 -0300

    :fire: removes unused useEffect import

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Redirect } from "react-router-dom";
 import styled from "styled-components";
 import PublicLayout from "../../components/global/PublicLayout/PublicLayout";
